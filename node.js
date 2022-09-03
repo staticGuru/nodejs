7c3189539365a270dff392e18da18db2c787820b
staticGuru <vigneshguru274@gmail.com>
2022-09-03 14:26:06 +0530

    rest operator

--- node.js
+++ node.js
@@ -1,3 +1,9 @@
 const fs= require('fs');
 fs.writeFileSync('helloworld.text','hellow world from the nodejs server');
 fs.writeFileSync('index.js','<div>guru</div>');
+
+
+const restOperator=(...args) =>{
+     return args
+}
+console.log(restOperator({"called":"q"},"name","guru",null))
