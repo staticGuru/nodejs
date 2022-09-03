134b88fc6ac5065a7f9c839e49ec124f82dec6a2
staticGuru <vigneshguru274@gmail.com>
2022-09-03 22:42:08 +0530

    redirecting url

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http=require('http');
+const fs=require('fs');
 http.createServer((req,res) => {
-     console.log("request sections",req.url,req.headers);
-     res.setHeader('Content-Type', 'text/html');
-     res.write('<html><head><title>Guruvignesh\'s server</title></head><body><p>the node js is running</p></body></html>');
-     res.end();
+     const url= req.url;
+     if(url == '/'){
+res.write("<html><body><form action'/messages' method='post' enctype='multipart/form'><input type='text'/><input type='submit'/></form></html>");
+return res.end();
+     }
+     if(url=='/messages'){
+      fs.writeFileSync('sample.txt',url);
+      res.statusCode=302;
+      res.setHeader('Location','/');
+      return res.end();
+
+     }
+  
 }).listen(3000,()=>console.log("Server started successfully"));
