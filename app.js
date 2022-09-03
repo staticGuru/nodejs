8f273a67ba8b2b1d15d9ae2643eb0f975202f8cd
staticGuru <vigneshguru274@gmail.com>
2022-09-03 19:14:31 +0530

    set the automate the commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const http=require('http');
 http.createServer((req,res) => {
-     console.log("request sections",req);
-     process.exit();
-}).listen(3000);
+     console.log("request sections",req.url,req.headers);
+}).listen(3000,()=>console.log("Server started successfully"));
