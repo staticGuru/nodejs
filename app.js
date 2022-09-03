325ba52f4f750ef7002763d2a04d49905beab8da
staticGuru <vigneshguru274@gmail.com>
2022-09-03 19:23:00 +0530

    set the html response

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const http=require('http');
 http.createServer((req,res) => {
      console.log("request sections",req.url,req.headers);
+     res.setHeader('Content-Type', 'text/html');
+     res.write('<html><head><title>Guruvignesh\'s server</title></head><body><p>the node js is running</p></body></html>');
+     res.end();
 }).listen(3000,()=>console.log("Server started successfully"));
