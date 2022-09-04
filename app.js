b73cfa7ee406250382604a20cf6fe00942943db1
staticGuru <vigneshguru274@gmail.com>
2022-09-04 13:43:12 +0530

    change the node server

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const http=require('http');
-const fs=require('fs');
-http.createServer((req,res) => {
-     const url= req.url;
-     if(url == '/'){
-res.write("<html><body><form action'/messages' method='post' enctype='multipart/form'><input type='text'/><input type='submit'/></form></html>");
-return res.end();
-     }
-     if(url=='/messages'){
-      fs.writeFileSync('sample.txt',url);
-      res.statusCode=302;
-      res.setHeader('Location','/');
-      return res.end();
+const http = require('http');
 
-     }
-  
-}).listen(3000,()=>console.log("Server started successfully"));
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
