ff879b7dae26f61180c3cc6ebd4a23ecf2f8654e
staticGuru <vigneshguru274@gmail.com>
2022-09-06 08:30:57 +0530

    setup the node js

--- project/app.js
+++ project/app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express =require('express');
+const app= express();
 
-console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+
+const server = http.createServer(app);
 
 server.listen(3000);
