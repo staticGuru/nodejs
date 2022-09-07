42ec52b64d07f12cd9a01d71b0f88dc4f3a2a99c
staticGuru <vigneshguru274@gmail.com>
2022-09-07 22:20:22 +0530

    change the app js file

--- project/app.js
+++ project/app.js
@@ -1,4 +1,4 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 const app = express();
@@ -11,6 +11,6 @@ app.use((req, res, next) => {
   console.log("another middleware called");
   res.send("<h1>Response from the middleware</h1>")
 });
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
