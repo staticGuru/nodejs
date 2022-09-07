0ecec689a9718468b630cf90208fd46f0ea44c70
staticGuru <vigneshguru274@gmail.com>
2022-09-07 08:28:52 +0530

    setup the middleware

--- project/app.js
+++ project/app.js
@@ -1,10 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const express =require('express');
-const app= express();
+const express = require("express");
+const app = express();
 
-
-
+app.use((req, res, next) => {
+  console.log("middleware called");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("another middleware called");
+  res.send("<h1>Response from the middleware</h1>")
+});
 const server = http.createServer(app);
 
 server.listen(3000);
