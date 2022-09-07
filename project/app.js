d8a1f1ce3a7200f2368fe2b26d466cff5ac9da66
staticGuru <vigneshguru274@gmail.com>
2022-09-07 22:34:34 +0530

    add the assignments

--- project/app.js
+++ project/app.js
@@ -3,13 +3,18 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
   console.log("middleware called");
+  // res.send("<h1>Response from the / page</h1>")
   next();
 });
-app.use((req, res, next) => {
+app.use('/users',(req, res, next) => {
   console.log("another middleware called");
-  res.send("<h1>Response from the middleware</h1>")
+  res.send("<h1>Response from users middleware</h1>")
+});
+app.use('/user',(req, res, next) => {
+  console.log("another middleware called");
+  res.send("<h1>Response from user middleware</h1>")
 });
 // const server = http.createServer(app);
 
